5ec846a35f607c8a1278bed388cb670a9b436298
Abhiram Vinjamuri <54580211+ramstar3000@users.noreply.github.com>
2025-01-12 10:46:06 +0000

    Basic Drawing

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Memorise
+
+Initial Sketch of the UI
+
+![Initial Sketch of the UI](static/sketch_excalidraw.png)
+Idea is that we have good startup animations to flip each tile and get a matches
+
+Tasks TODO
+
+- [x] Create a basic UI of boxes in JS
+- [x] Add a flip animation to the boxes, with images behind them
+- [x] Add concept of matches to do with game state
+
+
+- [x] Add a match animation, reward
---
+++ static/game.js
@@ -0,0 +1,43 @@
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped');
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    const isMatch = firstCard.dataset.card === secondCard.dataset.card;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.classList.add('matched');
+    secondCard.classList.add('matched');
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+
+        resetBoard();
+    }, 150);
+}
+
+function resetBoard() {
+    [firstCard, secondCard] = [null, null];
+    lockBoard = false;
+}
